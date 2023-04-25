7923d7311f7102839e3064dfa27cdc372ff25216
alexl8 <alex.besfam74@gmail.com>
2023-04-25 15:13:18 +0500

    layout\add component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="calc">
         <div className="wrapper">
+            <div className="calc-display">
+              <p>0</p>
+            </div>
+            <div className="buttons">
+              <button className="btn">AC</button>
+              <button className="btn">+/-</button>
+              <button className="btn">%</button>
+              <button className="btn bg-orange"></button>
 
+              <button className="btn">7</button>
+              <button className="btn">8</button>
+              <button className="btn">9</button>
+              <button className="btn bg-orange">/</button>
+
+              <button className="btn">4</button>
+              <button className="btn">5</button>
+              <button className="btn ">6</button>
+              <button className="btn bg-orange">x</button>
+
+              <button className="btn">1</button>
+              <button className="btn">2</button>
+              <button className="btn">3</button>
+              <button className="btn bg-orange">-</button>
+
+              <button className="btn">,</button>
+              <button className="btn">0</button>
+              <button className="btn">=</button>
+              <button className="btn bg-orange">+</button>
+            </div>
         </div>
     </div>
   );
---
+++ src/Components/Buttons/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Button = ({value}) => {
+  return (
+	<button className="btn">AC</button>
+  )
+}
+
+export default Button;
